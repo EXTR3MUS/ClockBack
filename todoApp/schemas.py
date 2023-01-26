64d0a327a655c42f76f009149d2974b1ff1cf867
EXTR3MUS <diego7042@gmail.com>
2023-01-26 18:22:18 -0300

    Creating base api with fastapi

---
+++ todoApp/schemas.py
@@ -0,0 +1,35 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+class TodoItemBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+class TodoItemCreate(TodoItemBase):
+    pass
+
+
+class TodoItem(TodoItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[TodoItem] = []
+
+    class Config:
+        orm_mode = True
